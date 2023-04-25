7fc568515d9357476e9697c0fe7d658d1426b351
Tom.Li <open222333@gmail.com>
2023-04-25 17:24:21 +0800

    1.05

--- src/crawler.py
+++ src/crawler.py
@@ -35,7 +35,9 @@ def videoConvertToTs(video_path: str, output_video_dir: str, output_video_name: 
     elif video_encoding == 'h265':
         encoding = 'libx265 -crf 28 -tune fastdecode'
     elif video_encoding == 'av1':
-        encoding = 'libaom-av1 -crf 30'
+        # encoding = 'libaom-av1 -crf 30'
+        # encoding = 'libsvtav1'
+        encoding = 'librav1e'
     else:
         return ''
 
